516dc162656d4c751d398f1e451f2bbaa4747e12
floooko <flo.x@hotmail.de>
2018-06-19 15:40:59 +0200

    Scripts optimized for delivery

--- models/goals_scored/knn.R
+++ models/goals_scored/knn.R
@@ -4,9 +4,10 @@ library(BBmisc)
 ### Prepare data and runs the model
 run.knn.regression <- function () {
   
-  # matches.merged.all.features = load.matches.with.all.features.for.match.result()
+  ## Note: If matches.merged.all.features is not already loaded to your environment, uncomment the following line
+  ## matches.merged.all.features = load.matches.with.all.features.for.match.result()
   
-  # Select features for predicting
+  ## Select features for predicting
   features.for.keeping <- c(
     'result',
     'season',
@@ -20,7 +21,7 @@ run.knn.regression <- function () {
     'win.ratio.away.team.playing.away'
   )
   
-  # select from dataframe only columns relevant for predicting
+  ## select from dataframe only columns relevant for predicting
   features.for.predicting <- features.for.keeping[3:length(features.for.keeping)]
   
   #normalize data and prepare home and away datasets
@@ -40,40 +41,53 @@ run.knn.regression <- function () {
     'win.ratio.away.team.playing.away'
   )
   
-  #predicted.goals.home <- make.knn.model (matches.for.training.home, features.for.predicting, 0.1, 107)
-  predicted.goals.home <- tune.knn.model (matches.for.training.home, features.for.predicting, 0.1, 100:200)
-  predicted.goals.away <- c()
+  ## Note: Comment and uncomment the following lines depending on whether predicting or tuning is required and
+  ##       whether home or away goals shall be predicted
   
-  return( c(predicted.goals.home, predicted.goals.away) )
+  ## PREDICT HOME
+  #prediction <- make.knn.model (matches.for.training.home, features.for.predicting, 0.1, 107)
+  
+  ## PREDICT AWAY
+  #prediction <- make.knn.model (matches.for.training.away, features.for.predicting, 0.1, 175)
+  
+  ## TUNE HOME
+  #prediction <- tune.knn.model (matches.for.training.home, features.for.predicting, 0.1, 100:200)
+  
+  ## TUNE AWAY
+  prediction <- tune.knn.model (matches.for.training.away, features.for.predicting, 0.1, 1:300)
+  
+  return(prediction)
 }
 
 ### This function creates the model and predicts goals using the optimal parameter k (found in model tuning)
 ### depending on the column copied into "target" this function predicts the goals for the home or away team
-### K can be set when calling the function, default is 1
+### Parameters:
+###     matches     - pass the dataset for training and testing
+###     test.method - pass either "last.season" or percentage for test data (e.g. 0.1)
+###     k           - pass an integer for k; default is 1
+### Return:
+###     The function returns a "result" object containing the model, predicted goals, nrmse and accuracy  
 make.knn.model <- function(matches, features.for.predicting, test.method, k = 1) {
   
   matches <- matches[,features.for.predicting]
   
-  # spliting date in training and test data
+  ## spliting date in training and test data
   index.te = if (test.method == "last.season") which(matches$season == "2015/2016") else 
     sample(seq_len(nrow(matches)),size=nrow(matches)*test.method)
   
   matches.train = matches[-index.te,]
   matches.test = matches[index.te,]
   
-  # create and train the model
+  ## create and train the model
   pred_001 = FNN::knn.reg(train = matches.train[,-which(names(matches.train)=="target")], test = matches.test[,-which(names(matches.test)=="target")], y = matches.train$target, k = k)
-  
   predicted.goals <- pred_001$pred
   
+  ## calculate nrmse and accuracy
   nrmse.knn <-  (mean((matches.test$target - predicted.goals)^2)/var(matches.test$target))^0.5
   accuracy.knn <- mean(matches.test$target == round(predicted.goals))
   
-
-  print(cbind(matches.test$target, round(predicted.goals)))
-  
-  print(nrmse.knn)
-  print(accuracy.knn)
+  print(paste0("nrmse:     ", nrmse.knn))
+  print(paste0("accuracy:  ", accuracy.knn))
   
   result = vector(mode="list")
   result[["model"]] <- pred_001
@@ -86,19 +100,24 @@ make.knn.model <- function(matches, features.for.predicting, test.method, k = 1)
 
 ### This function tunes the models parameter k and runs the model afterwards
 ### depending on the column copied into "target" this function predicts the goals for the home or away team
-### Additional to the values retunred by make.knn.model this function retunrs the best k found
+### Parameters:
+###     matches     - pass the dataset for training and testing
+###     test.method - pass either "last.season" or percentage for test data (e.g. 0.1)
+###     k.values    - pass either an integer or a range (e.g. 1:50); default is 1
+### Return:
+###     Additional to the values retunred by make.knn.model this function retunrs the best k found
 tune.knn.model <- function(matches, features.for.predicting, test.method, k.values) {
   
   matches <- matches[,features.for.predicting]
   
-  # spliting date in training and test data
+  ## spliting date in training and test data
   index.te = if (test.method == "last.season") which(matches$season == "2015/2016") else 
     sample(seq_len(nrow(matches)),size=nrow(matches)*test.method)
   
   matches.train = matches[-index.te,]
   matches.test = matches[index.te,]
   
-  # create and train the model
+  ## create and train the model
   k.errors <- c()
   
   for(k in k.values){
@@ -108,24 +127,21 @@ tune.knn.model <- function(matches, features.for.predicting, test.method, k.valu
   }
   
   best.k <- which.min(k.errors)
+  print(paste0("best k:    ", best.k))
   
-  print(best.k)
+  ## create and run model with best k found before
+  pred = FNN::knn.reg(train = matches.train[,-which(names(matches.train)=="target")], test = matches.test[,-which(names(matches.test)=="target")], y = matches.train$target, k = best.k)
+  predicted.goals <- pred$pred
   
-  pred_001 = FNN::knn.reg(train = matches.train[,-which(names(matches.train)=="target")], test = matches.test[,-which(names(matches.test)=="target")], y = matches.train$target, k = best.k)
-  
-  predicted.goals <- pred_001$pred
-  
+  ## calculate nrmse and accuracy
   nrmse.knn <-  (mean((matches.test$target - predicted.goals)^2)/var(matches.test$target))^0.5
   accuracy.knn <- mean(matches.test$target == round(predicted.goals))
   
-  
-  #print(cbind(matches.test$target, round(predicted.goals)))
-  
-  print(nrmse.knn)
-  print(accuracy.knn)
+  print(paste0("nrmse:     ", nrmse.knn))
+  print(paste0("accuracy:  ", accuracy.knn))
   
   result = vector(mode="list")
-  result[["model"]] <- pred_001
+  result[["model"]] <- pred
   result[["predictions"]] <- predicted.goals
   result[["nrmse"]] <- nrmse.knn
   result[["accuracy"]] <- accuracy.knn
--- models/goals_scored/svregression.R
+++ models/goals_scored/svregression.R
@@ -1,11 +1,12 @@
 library(e1071)
 
 ### Prepare data and runs the model
-run.svr.regression <- function () {
-
-  # matches.merged.all.features = load.matches.with.all.features.for.match.result()
+run.svregression <- function () {
   
-  # Select features for predicting
+  ## Note: If matches.merged.all.features is not already loaded to your environment, uncomment the following line
+  ##       matches.merged.all.features = load.matches.with.all.features.for.match.result()
+  
+  ## Select features for predicting
   features.for.keeping <- c(
     'result',
     'season',
@@ -19,7 +20,7 @@ run.svr.regression <- function () {
     'win.ratio.away.team.playing.away'
   )
   
-  # select from dataframe only columns relevant for predicting
+  ## select from dataframe only columns relevant for predicting
   features.for.predicting <- features.for.keeping[3:length(features.for.keeping)]
   
   matches.for.training.home <- matches.merged.all.features[,features.for.keeping]
@@ -38,10 +39,22 @@ run.svr.regression <- function () {
     'win.ratio.away.team.playing.away'
   )
   
-  predicted.goals.home <- make.svregression.model (matches.for.training.home, features.for.predicting, 0.1)
-  predicted.goals.away <- make.svregression.model (matches.for.training.away, features.for.predicting, 0.1)
+  ## Note: Comment and uncomment the following lines depending on whether predicting or tuning is required and
+  ##       whether home or away goals shall be predicted
   
-  return( c(predicted.goals.home, predicted.goals.away) )
+  ## PREDICT HOME
+  prediction <- make.svregression.model (matches.for.training.home, features.for.predicting, 0.1)
+  
+  ## PREDICT AWAY
+  #prediction <- make.svregression.model (matches.for.training.away, features.for.predicting, 0.1)
+  
+  ## TUNE HOME
+  #prediction <- tune.svregression.model (matches.for.training.home, features.for.predicting, 0.1)
+  
+  ## TUNE AWAY
+  #prediction <- tune.svregression.model (matches.for.training.away, features.for.predicting, 0.1)
+  
+  return( prediction )
 }
 
 ### This function creates the model and predicts goals using the optimal parameters (found in previous model tuning)
@@ -50,26 +63,25 @@ make.svregression.model <- function(matches, features.for.predicting, test.metho
   
   matches <- matches[,features.for.predicting]
   
-  # spliting date in training and test data
+  ## spliting date in training and test data
   index.te = if (test.method == "last.season") which(matches$season == "2015/2016") else 
     sample(seq_len(nrow(matches)),size=nrow(matches)*test.method)
   
   matches.train = matches[-index.te,]
   matches.test = matches[index.te,]
   
-  # create and train the model
+  ## create and train the model
   model.svr <- svm(target ~., data = matches.train, epsilon = 0.6, cost=2)
  
-  # predict goals
+  ## predict goals
   predicted.goals <- predict(model.svr, data = matches.test)
   
-  # calculate error end accuracy
-  
+  ## calculate error end accuracy
   nrmse.svr <-  (mean((matches.test$target - predicted.goals)^2)/var(matches.test$target))^0.5
   accuracy.svr <- mean(matches.test$target == round(predicted.goals))
   
-  print(nrmse.svr)
-  print(accuracy.svr)
+  print(paste0("nrmse:     ", nrmse.svr))
+  print(paste0("accuracy:  ", accuracy.svr))
   
   result = vector(mode="list")
   result[["model"]] <- model.svr
@@ -87,29 +99,32 @@ tune.svregression.model <- function(matches, features.for.predicting, test.metho
 
   matches <- matches[,features.for.predicting]
   
-  # spliting date in training and test data
+  ## spliting date in training and test data
   index.te = if (test.method == "last.season") which(matches$season == "2015/2016") else 
     sample(seq_len(nrow(matches)),size=nrow(matches)*test.method)
   
   matches.train = matches[-index.te,]
   matches.test = matches[index.te,]
   
-  # tuning of the model
+  ## tuning of the model
   tuneResult <- tune(svm, target ~., data = matches.train,
                      ranges = list(epsilon = seq(0,1,0.2), cost = 2^(1:5))
   )
   
   print(tuneResult)
   
+  ## select and run the  best model found before
   model.svr <- tuneResult$best.model
   
+  ## predict goals
   predicted.goals <- predict(tunedModel, data = matches.test) 
   
+  ## calculate error end accuracy
   nrmse.svr <-  (mean((matches.test$target - predicted.goals)^2)/var(matches.test$target))^0.5
   accuracy.svr <- mean(matches.test$target == round(predicted.goals))
   
-  print(nrmse.svr)
-  print(accuracy.svr)
+  print(paste0("nrmse:     ", nrmse.svr))
+  print(paste0("accuracy:  ", accuracy.svr))
   
   result = vector(mode="list")
   result[["model"]] <- model.svr
